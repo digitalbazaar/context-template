90f2cc88e86fa7d839f1a4131c6ee506aa8891f5
Dmitri Zagidulin <dzagidulin@gmail.com>
2021-04-14 16:43:09 -0400

    Refactor, serialize jsonld on build.

--- js/constants.js
+++ js/constants.js
@@ -4,6 +4,7 @@
 'use strict';
 
 module.exports = {
+  CONTEXT_FILENAME: 'example-v1.jsonld',
   CONTEXT_URL: 'https://w3id.org/example/v1',
   // value between 0x0 and 0x7FFF for globally registered term codec values
   // or >= 0x8000 for app-specific local terms
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   const bundler = process.env.BUNDLER || 'webpack';
   const frameworks = ['mocha', 'chai'];
 
-  const files = ['unit/*.js'];
+  const files = ['*.spec.js'];
 
   // browser launchers: https://npmjs.org/browse/keyword/karma-launcher
   // browsers: ['ChromeHeadless', 'Chrome', 'Firefox', 'Safari'],
@@ -47,7 +47,7 @@ module.exports = function(config) {
     // available preprocessors:
     // https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'unit/*.js': preprocessors,
+      '*.spec.js': preprocessors,
     },
 
     webpack: {
@@ -56,7 +56,6 @@ module.exports = function(config) {
       node: {
         Buffer: false,
         crypto: false,
-        setImmediate: false,
         util: false
       }
     }

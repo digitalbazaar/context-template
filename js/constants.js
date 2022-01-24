be27f988f52c402c12f2ebef25af251c003f55be
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-01-24 14:50:07 -0500

    Update export constant variable names.

--- js/constants.js
+++ js/constants.js
@@ -5,7 +5,7 @@
 
 module.exports = {
   CONTEXT_FILENAME: 'example-v1.jsonld',
-  CONTEXT_URL: 'https://w3id.org/example/v1',
+  CONTEXT_URL_V1: 'https://w3id.org/example/v1',
   // value between 0x0 and 0x7FFF for globally registered term codec values
   // or >= 0x8000 for app-specific local terms
   CBORLD_VALUE: 0x0
